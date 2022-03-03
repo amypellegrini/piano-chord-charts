e8778d45450de267316e5f8fad7d0d309b08afa5
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-03-03 08:14:48 +0000

    refactor(template): adjust SVG viewbox and keys relative distance to start from zero

--- src/template.ts
+++ src/template.ts
@@ -12,27 +12,22 @@ const defaultContent = `<rect style="fill:#fafafa;stroke:black" x="0" y="0" widt
 <rect style="fill:#fafafa;stroke:black" x="253" y="0" width="23" height="120" ry="3"></rect>
 <rect style="fill:#fafafa;stroke:black" x="276" y="0" width="23" height="120" ry="3"></rect>
 <rect style="fill:#fafafa;stroke:black" x="299" y="0" width="23" height="120" ry="3"></rect>
-<rect style="fill:#fafafa;stroke:black" x="322" y="0" width="23" height="120" ry="3"></rect>
-<rect style="fill:#fafafa;stroke:black" x="345" y="0" width="23" height="120" ry="3"></rect>
-<rect style="fill:#fafafa;stroke:black" x="368" y="0" width="23" height="120" ry="3"></rect>
-<rect style="fill:#fafafa;stroke:black" x="391" y="0" width="23" height="120" ry="3"></rect>
-<rect style="fill:black;stroke:black" x="19.75" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="60.333330000000004" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="87.66666000000001" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="128.25" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="154.25" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="180.75" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="221.33333" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="248.66666" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="289.25" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="315.25" y="0" width="13" height="80" ry="1"></rect>
-<rect style="fill:black;stroke:black" x="341.75" y="0" width="13" height="80" ry="1"></rect>
-<rect y="-1" width="1196" height="3"></rect>`;
+<rect style="fill:black;stroke:black" x="14.33333" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="41.66666" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="82.25" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="108.25" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="134.75" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="175.33333" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="202.66666" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="243.25" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="269.25" y="0" width="13" height="80" ry="1"></rect>
+<rect style="fill:black;stroke:black" x="295.75" y="0" width="13" height="80" ry="1"></rect>
+<rect y="0" width="322" height="3"></rect>`;
 
 function render(content?: string) {
   const template = `<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-  <svg width="322" height="120" viewBox="46 0 322 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <svg width="322" height="120" viewBox="0 0 322 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
     ${content || defaultContent}
   </svg>`;
 
