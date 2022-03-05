680e39f6a01f34d0cd2157bdf72ec3a9e958862c
Amy Pellegrini <amyvpellegrini@gmail.com>
2022-03-05 08:39:44 +0000

    feat(template): add support for compact and exact template render

--- src/__tests__/template.test.ts
+++ src/__tests__/template.test.ts
@@ -7,13 +7,24 @@ describe("default chart template", () => {
 });
 
 describe("render", () => {
-  it("renders a default chart when no args are given", () => {
+  it("renders a default compact chart when no options are given", () => {
     const result = render();
     expect(result).toMatchSnapshot();
   });
 
-  it("renders gvien content inside the template", () => {
-    const result = render("text content");
-    expect(result).toMatchSnapshot();
+  it("renders compact and exact versions of the keyboard", () => {
+    const defaultRender = render();
+
+    const withCompactOption = render({
+      format: "compact",
+    });
+
+    const withExactOption = render({
+      format: "exact",
+    });
+
+    expect(defaultRender).toEqual(withCompactOption);
+    expect(defaultRender).not.toEqual(withExactOption);
+    expect(withExactOption).toMatchSnapshot();
   });
 });
